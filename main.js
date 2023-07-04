7c4e4cf24e2c428ac6b2b156e2c5912e6124d826
Burdusa Daniel <danielburdusa26@gmail.com>
2023-07-04 15:40:40 +0200

    finished product with bonus

--- main.js
+++ main.js
@@ -8,12 +8,35 @@ Consigli del giorno:
 2. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano" anche su un foglietto
 Buon lavoro e buon divertimento! */
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
     if (i % 3 == 0 && i % 5 == 0) {
         console.log("FizzBuzz");
+        document.getElementById('squaresContainer').innerHTML += `
+        <div class="square bg-danger">
+            <p class="text-center text-white fs-3 fw-3 vertical-align-square">${"FitzBuzz"}</p>
+        </div>
+    `;
     } else if (i % 3 == 0) {
         console.log("Fizz");
+        document.getElementById('squaresContainer').innerHTML += `
+        <div class="square bg-success">
+            <p class="text-center text-white fs-3 fw-3 vertical-align-square">${"Fizz"}</p>
+        </div>
+    `;
     } else if (i % 5 == 0) {
         console.log("Buzz");
+        document.getElementById('squaresContainer').innerHTML += `
+        <div class="square bg-warning">
+            <p class="text-center text-white fs-3 fw-3 vertical-align-square">${"Buzz"}</p>
+        </div>
+    `;
+    } else {
+        console.log(i);
+        document.getElementById('squaresContainer').innerHTML += `
+        <div class="square  ">
+            <p class="text-center text-white fs-3 fw-3 vertical-align-square">${i}</p>
+        </div>
+    `;
     }
+
 }
+
